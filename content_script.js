1ac287dc94f94a8ea782adc2fe3a400e3cd57fb7
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2021-05-16 18:48:16 +0100

    intial commit

---
+++ content_script.js
@@ -0,0 +1,121 @@
+/* This runs on all "youtube.com/watch" web pages */
+console.log("----- [content_script.js] LOADED");
+
+load_button();
+
+function load_button() {
+	// Create Screen Shot Button
+	var ss_btn = document.createElement("button");
+	ss_btn.innerHTML = "Anki";
+	ss_btn.className = "btn_save_screenshot ytp-button";
+	ss_btn.style.width = "auto";
+	// ss_btn.style.cssFloat = "left";
+	ss_btn.onclick = send_to_anki;
+	document.getElementsByClassName("ytp-left-controls")[0].appendChild(ss_btn);
+
+	console.log("Loaded Button");
+}
+
+function send_to_anki() {
+	console.log("\nSending to ANKI");
+
+	var canvas = document.createElement("canvas");
+	var video = document.querySelector("video");
+	var ctx = canvas.getContext("2d");
+
+	// Change the size here
+	canvas.width = 640;
+	canvas.height = 360;
+
+	ctx.drawImage(video, 0, 0, 640, 360);
+
+	var dataURL = canvas.toDataURL("image/png");
+	dataURL = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+
+	/* making time stamped url */
+	var short_url = document.querySelectorAll('[rel="shortlinkUrl"]')[0].href;
+	var videoId = document.querySelectorAll('[itemprop="videoId"]')[0].content;
+	var current_time = document.querySelector(".video-stream").currentTime.toFixed();
+	var youtube_url = short_url + "?t=" + current_time;
+
+	/* screenshot file name */
+	const imageFilename = "Youtube2Anki_" + canvas.width + "x" + canvas.height + "-" + videoId + "_" + current_time + ".png";
+
+	var youtube_share_url = "https://youtu.be/" + videoId + "?t=" + current_time; /* https://youtu.be/RksaXQ4C1TA */
+	console.log("VIDEOID =", videoId)
+	console.log(youtube_share_url)
+	console.log(youtube_url)
+
+	console.log("loading user settings")
+	chrome.storage.local.get(["ankiDeckNameSel", "ankiModelNameSel", "ankiFieldScreenshot", "ankiFieldURL",],
+		({ ankiDeckNameSel, ankiModelNameSel, ankiFieldScreenshot, ankiFieldURL, ankiConnectUrl, }) => {
+
+			url = ankiConnectUrl || "http://localhost:8765/";
+			model = ankiModelNameSel || "Basic";
+			deck = ankiDeckNameSel || "Default";
+
+			var fields = {
+				[ankiFieldScreenshot]: '<img src="' + imageFilename + '" />',
+				[ankiFieldURL]: "<a href=" + youtube_share_url + ">" + youtube_share_url + "</a>"
+			};
+
+			var card_data = {
+				"action": "multi",
+				"params": {
+					"actions": [
+						{
+							"action": "storeMediaFile",
+							"params": {
+								"filename": imageFilename,
+								"data": dataURL,
+							},
+						},
+						{
+							"action": "addNote",
+							"params": {
+								"note": {
+									"modelName": model,
+									"deckName": deck,
+									"fields": fields,
+									"tags": ["Youtube2Anki"],
+								},
+							},
+						},
+					],
+				},
+			};
+
+			var permission_data = {
+				"action": "requestPermission",
+				"version": 6,
+			};
+
+			console.log("Fetching...")
+
+			fetch(url, {
+				method: "POST",
+				body: JSON.stringify(permission_data),
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					console.log("Permission Granted")
+					console.log(data);
+
+					fetch(url, {
+						method: "POST",
+						body: JSON.stringify(card_data),
+					})
+						.then((res) => res.json())
+						.then((data) => {
+							console.log("Fetch Return:")
+							if (data.result === null) {
+								alert("Error!\n" + data.error)
+							}
+							console.log("Sucess")
+						})
+						.catch((error) => console.log(error));
+				});
+			console.log("Sent to ANKI complete!\n");	
+		}
+	);
+}
---
+++ popup/popup.js
@@ -0,0 +1,77 @@
+
+var fetchOptions = function (selectId, url, action, params = {}) {
+	var selectEl = document.getElementById(selectId);
+
+	return new Promise((resolve, reject) => {
+		chrome.storage.local.get(selectId, (stored) => {
+			var storedVal = stored[selectId] || 'Basic';
+			fetch(url, { method: "POST", body: JSON.stringify({ "action": action, "params": params }) }).then(r => r.json()).then(data => {
+				data.forEach((item) => {
+					e = document.createElement("option");
+					e.value = item;
+					e.text = item;
+					if (item === storedVal) e.selected = true;
+					selectEl.appendChild(e);
+				})
+				if (action === "modelFieldNames") {
+					e = document.createElement("option");
+					e.value = " ";
+					e.text = " ";
+					if (storedVal === " ") e.selected = true;
+					selectEl.add(e);
+				}
+
+			}).then(r => resolve(r)).catch(e => reject(e));
+		});
+	});
+}
+
+var saveOption = function (selectId) {
+	var selectEl = document.getElementById(selectId);
+	return chrome.storage.local.set({ [selectId]: selectEl.value })
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	var urlEl = document.getElementById('ankiConnectUrl');
+	var model_Name = document.getElementById('ankiNoteNameSel');
+	var submit_button = document.getElementById('saveAnkiBtn');
+
+	chrome.storage.local.get('ankiConnectUrl', ({ ankiConnectUrl }) => {
+		var url = ankiConnectUrl || 'http://localhost:8765';
+		urlEl.classList.add('focused');
+		urlEl.value = url;
+
+		Promise.all([
+			/* Get All Deck names and all Note Types */
+			fetchOptions('ankiDeckNameSel', url, 'deckNames'),
+			fetchOptions('ankiNoteNameSel', url, 'modelNames') /* note type */
+		]).then(() => {
+			/* Then we get all the Field's for the selected Note type */
+			/*      dont change 'modelFieldNames' - this is for ankiconnect */
+			fetchOptions('ankiFieldScreenshot', url, 'modelFieldNames', { "modelName": model_Name.value })
+			fetchOptions('ankiFieldURL', url, 'modelFieldNames', { "modelName": model_Name.value })
+
+			model_Name.addEventListener("change", function () {
+				console.log("-- CHANGE")
+				var array = ["ankiFieldScreenshot", "ankiFieldURL"];
+				array.forEach((item) => {
+					document.getElementById(item).length = 0;
+					fetchOptions(item, url, 'modelFieldNames', { "modelName": model_Name.value });
+				})
+			});
+
+			submit_button.addEventListener('click', (e) => {
+				console.log("-- CLICK")
+				Promise.all([
+					saveOption('ankiDeckNameSel'),
+					saveOption('ankiNoteNameSel'),
+
+					saveOption('ankiFieldScreenshot'),
+					saveOption('ankiFieldURL')
+				])
+					.then(() => alert(`Options saved!`))
+					.catch(error => alert(`Cannot save options: ${error}`))
+			});
+		}).catch(error => alert(`Cannot fetch options via AnkiConnect: ${error}`))
+	})
+})
