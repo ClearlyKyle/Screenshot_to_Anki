561fd550974e3df1622099935dc5cda0d5ac7a1b
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2021-12-05 23:13:46 +0000

    fixed possible loading issue

--- chrome/content_script.js
+++ chrome/content_script.js
@@ -1,186 +1,190 @@
-/* This runs on all "youtube.com/watch" web pages */
-console.log("----- [content_script.js] LOADED");
 
-load_button();
-
-const notification_toast_time = 3000;
-
-function load_button()
+(function ()
 {
-	// Create Screen Shot Button
-	var ss_btn = document.createElement("button");
-	ss_btn.innerHTML = "Anki";
-	ss_btn.className = "btn_save_screenshot ytp-button";
-	ss_btn.style.width = "auto";
-	ss_btn.style.cssFloat = "left";
-	ss_btn.style.marginRight = "10px";
-	ss_btn.onclick = send_to_anki;
-	document.getElementsByClassName("ytp-right-controls")[0].prepend(ss_btn);
+	/* This runs on all "youtube.com/watch" web pages */
+	console.log("----- [content_script.js] LOADED");
 
-	console.log("Loaded Button");
-}
+	load_button();
 
-function send_to_anki()
-{
-	console.log("\nSending to ANKI");
+	const notification_toast_time = 3000;
 
-	var canvas = document.createElement("canvas");
-	var video = document.querySelector("video");
-	var ctx = canvas.getContext("2d");
+	function load_button()
+	{
+		// Create Screen Shot Button
+		var ss_btn = document.createElement("button");
+		ss_btn.innerHTML = "Anki";
+		ss_btn.className = "btn_save_screenshot ytp-button";
+		ss_btn.style.width = "auto";
+		ss_btn.style.cssFloat = "left";
+		ss_btn.style.marginRight = "10px";
+		ss_btn.onclick = send_to_anki;
+		document.getElementsByClassName("ytp-right-controls")[0].prepend(ss_btn);
 
-	// Change the size here
-	canvas.width = 640;
-	canvas.height = 360;
+		console.log("Loaded Button");
+	}
 
-	ctx.drawImage(video, 0, 0, 640, 360);
+	function send_to_anki()
+	{
+		console.log("\nSending to ANKI");
 
-	var dataURL = canvas.toDataURL("image/png");
-	dataURL = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+		var canvas = document.createElement("canvas");
+		var video = document.querySelector("video");
+		var ctx = canvas.getContext("2d");
 
-	/* making time stamped url */
-	var short_url = document.querySelectorAll('[rel="shortlinkUrl"]')[0].href;
-	var videoId = document.querySelectorAll('[itemprop="videoId"]')[0].content;
-	var current_time = document.querySelector(".video-stream").currentTime.toFixed();
-	var youtube_url = short_url + "?t=" + current_time;
+		// Change the size here
+		canvas.width = 640;
+		canvas.height = 360;
 
-	/* screenshot file name */
-	const imageFilename = "Youtube2Anki_" + canvas.width + "x" + canvas.height + "-" + videoId + "_" + current_time + ".png";
+		ctx.drawImage(video, 0, 0, 640, 360);
 
-	var youtube_share_url = "https://youtu.be/" + videoId + "?t=" + current_time; /* https://youtu.be/RksaXQ4C1TA */
-	console.log("VIDEOID =", videoId)
-	console.log(youtube_share_url)
-	console.log(youtube_url)
+		var dataURL = canvas.toDataURL("image/png");
+		dataURL = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 
-	console.log("loading user settings")
-	chrome.storage.local.get(["ankiDeckNameSel", "ankiNoteNameSel", "ankiFieldScreenshot", "ankiFieldURL", "ankiConnectUrl"],
-		({ ankiDeckNameSel, ankiNoteNameSel, ankiFieldScreenshot, ankiFieldURL, ankiConnectUrl }) =>
-		{
+		/* making time stamped url */
+		var short_url = document.querySelectorAll('[rel="shortlinkUrl"]')[0].href;
+		var videoId = document.querySelectorAll('[itemprop="videoId"]')[0].content;
+		var current_time = document.querySelector(".video-stream").currentTime.toFixed();
+		var youtube_url = short_url + "?t=" + current_time;
 
-			console.log("local.get Data:")
-			console.log({ ankiDeckNameSel, ankiNoteNameSel, ankiFieldScreenshot, ankiFieldURL, ankiConnectUrl })
+		/* screenshot file name */
+		const imageFilename = "Youtube2Anki_" + canvas.width + "x" + canvas.height + "-" + videoId + "_" + current_time + ".png";
 
-			url = ankiConnectUrl || "http://localhost:8765/";
-			model = ankiNoteNameSel || "Basic";
-			deck = ankiDeckNameSel || "Default";
+		var youtube_share_url = "https://youtu.be/" + videoId + "?t=" + current_time; /* https://youtu.be/RksaXQ4C1TA */
+		console.log("VIDEOID =", videoId)
+		console.log(youtube_share_url)
+		console.log(youtube_url)
 
-			var fields = {
-				[ankiFieldScreenshot]: '<img src="' + imageFilename + '" />',
-				[ankiFieldURL]: "<a href=" + youtube_share_url + ">" + youtube_share_url + "</a>"
-			};
+		console.log("loading user settings")
+		chrome.storage.local.get(["ankiDeckNameSel", "ankiNoteNameSel", "ankiFieldScreenshot", "ankiFieldURL", "ankiConnectUrl"],
+			({ ankiDeckNameSel, ankiNoteNameSel, ankiFieldScreenshot, ankiFieldURL, ankiConnectUrl }) =>
+			{
 
-			var card_data = {
-				"action": "multi",
-				"params": {
-					"actions": [
-						{
-							"action": "storeMediaFile",
-							"params": {
-								"filename": imageFilename,
-								"data": dataURL,
-							},
-						},
-						{
-							"action": "addNote",
-							"params": {
-								"note": {
-									"modelName": model,
-									"deckName": deck,
-									"fields": fields,
-									"options": {
-										"allowDuplicate": false
-									},
-									"tags": ["Youtube2Anki"],
+				console.log("local.get Data:")
+				console.log({ ankiDeckNameSel, ankiNoteNameSel, ankiFieldScreenshot, ankiFieldURL, ankiConnectUrl })
+
+				url = ankiConnectUrl || "http://localhost:8765/";
+				model = ankiNoteNameSel || "Basic";
+				deck = ankiDeckNameSel || "Default";
+
+				var fields = {
+					[ankiFieldScreenshot]: '<img src="' + imageFilename + '" />',
+					[ankiFieldURL]: "<a href=" + youtube_share_url + ">" + youtube_share_url + "</a>"
+				};
+
+				var card_data = {
+					"action": "multi",
+					"params": {
+						"actions": [
+							{
+								"action": "storeMediaFile",
+								"params": {
+									"filename": imageFilename,
+									"data": dataURL,
 								},
 							},
-						},
-					],
-				},
-			};
+							{
+								"action": "addNote",
+								"params": {
+									"note": {
+										"modelName": model,
+										"deckName": deck,
+										"fields": fields,
+										"options": {
+											"allowDuplicate": false
+										},
+										"tags": ["Youtube2Anki"],
+									},
+								},
+							},
+						],
+					},
+				};
 
-			console.log("Card Data...")
-			console.log(card_data)
+				console.log("Card Data...")
+				console.log(card_data)
 
-			var permission_data = {
-				"action": "requestPermission",
-				"version": 6,
-			};
+				var permission_data = {
+					"action": "requestPermission",
+					"version": 6,
+				};
 
-			console.log("Fetching...")
+				console.log("Fetching...")
 
-			fetch(url, {
-				method: "POST",
-				body: JSON.stringify(permission_data),
-			})
-				.then((res) => res.json())
-				.then((data) =>
-				{
-					if (data.error !== null)
+				fetch(url, {
+					method: "POST",
+					body: JSON.stringify(permission_data),
+				})
+					.then((res) => res.json())
+					.then((data) =>
 					{
-						console.log("Permission Failed")
-						console.log(data);
-						return
-					}
-					console.log("Permission Granted")
-					console.log(data);
-
-					fetch(url, {
-						method: "POST",
-						body: JSON.stringify(card_data),
-					})
-						.then((res) => res.json())
-						.then((data) =>
+						if (data.error !== null)
 						{
-                            console.log("Fetch Return:")
-                            console.log(data)
-                            if (data.result === null)
-                            {
-                                // https://jsfiddle.net/2qasgcfd/3/
-                                // https://github.com/apvarun/toastify-js
-                                Toastify({
-                                    text: "Error! " + data,
-                                    duration: notification_toast_time,
-                                    style: {
-                                        background: "red",
-                                    }
-                                }).showToast();
-                                return
-                            }
-                            else
-                            {
-                                /* show sucess message */
-                                Toastify({
-                                    text: "Sucessfully added to ANKI",
-                                    duration: notification_toast_time,
-                                    style: {
-                                        background: "light blue",
-                                    }
-                                }).showToast();
-                            }
-                        })
-                        .catch((error) =>
-                        {
-                            /* show error message */
-                            Toastify({
-                                text: "Error! " + error,
-                                duration: notification_toast_time,
-                                style: {
-                                    background: "red",
-                                }
-                            }).showToast();
-                        })
-                }).catch((error) =>
-                {
-                    /* show error message */
-                    Toastify({
-                        text: "Error! " + error,
-                        duration: notification_toast_time,
-                        style: {
-                            background: "red",
-                        }
-                    }).showToast();
-                    console.log(error)
-                });
-            console.log("Send to ANKI complete!\n");
-        }
-    );
-}
+							console.log("Permission Failed")
+							console.log(data);
+							return
+						}
+						console.log("Permission Granted")
+						console.log(data);
+
+						fetch(url, {
+							method: "POST",
+							body: JSON.stringify(card_data),
+						})
+							.then((res) => res.json())
+							.then((data) =>
+							{
+								console.log("Fetch Return:")
+								console.log(data)
+								if (data.result === null)
+								{
+									// https://jsfiddle.net/2qasgcfd/3/
+									// https://github.com/apvarun/toastify-js
+									Toastify({
+										text: "Error! " + data,
+										duration: notification_toast_time,
+										style: {
+											background: "red",
+										}
+									}).showToast();
+									return
+								}
+								else
+								{
+									/* show sucess message */
+									Toastify({
+										text: "Sucessfully added to ANKI",
+										duration: notification_toast_time,
+										style: {
+											background: "light blue",
+										}
+									}).showToast();
+								}
+							})
+							.catch((error) =>
+							{
+								/* show error message */
+								Toastify({
+									text: "Error! " + error,
+									duration: notification_toast_time,
+									style: {
+										background: "red",
+									}
+								}).showToast();
+							})
+					}).catch((error) =>
+					{
+						/* show error message */
+						Toastify({
+							text: "Error! " + error,
+							duration: notification_toast_time,
+							style: {
+								background: "red",
+							}
+						}).showToast();
+						console.log(error)
+					});
+				console.log("Send to ANKI complete!\n");
+			}
+		);
+	}
+})
